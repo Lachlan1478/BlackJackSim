9b52ef4d5c55d5274e577f437969fe34a36a5a48
Lachy1478 <gaylerlachlan@gmail.com>
2022-06-20 18:00:39 +0930

    Adding Code Files

--- BlackJackEngine.py
+++ BlackJackEngine.py
@@ -1,21 +1,28 @@
 import random
 
 
+def drawCard():
+    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    index = random.randint(0, 12)
+    card = cards[index]
+    return card
+
 class Player():
-    def drawCard(self):
-        cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        index = random.randint(0, 12)
-        card = cards[index]
-        return card
     def hit(self):
-        newCard = self.drawCard()
-        pass
+        newCard = drawCard()
+        self.sum += newCard
+
     def split(self):
         pass
     def double(self):
         pass
     def stand(self):
         pass
+    def HandValue(self):
+        if(self.sum > 21):
+            return 0
+        else:
+            return self.sum
     def Blackjack(self):
         pass
 
@@ -23,9 +30,48 @@ class Player():
         self.card1 = card1
         self.card2 = card2
         self.dealerscard = dealersCard
-
         self.sum = card1 + card2
-
 
         while(self.sum < 11):
             self.hit()
+
+        print(self.sum)
+
+        self.stand()
+
+
+class Game():
+    def getResult(self):
+        # Standoff
+        print(self.dealersHand)
+        if (self.PlayerHand == 0):
+            self.result = -1
+            return self.result
+        if (self.dealersHand == 22):
+            self.result = 0
+
+        else:
+            if (self.dealersHand > self.PlayerHand):
+                self.result = -1
+            elif (self.dealersHand == self.PlayerHand):
+                self.result = 0
+            else:
+                self.result = 1
+        return self.result
+
+    def __init__(self):
+        ## Deal cards
+        # Dealers Card
+        self.dealersCard = drawCard()
+        self.Player = Player(drawCard(), drawCard(), self.dealersCard)
+        self.PlayerHand = self.Player.HandValue()
+
+        self.dealersHand = self.dealersCard
+        while(self.dealersHand < 17):
+            self.dealersHand += drawCard()
+
+        #-1 if split or double loss, -1 if loss, 0 if draw, 1 if won and 2 if double or split win
+        self.result =  0
+
+
+
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import numpy
-import random
-from BlackJackEngine import Player
-
-def drawCard():
-    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    index = random.randint(0, 12)
-    card = cards[index]
-    return card
+from BlackJackEngine import Game
 
 def main():
     profit = 0
     bet = 10
 
-    card1 = drawCard()
-    dealersCard1 = drawCard()
-    card2 = drawCard()
-
-    #blackjack
-    if((card1 == 1 and card2 == 10) or (card1 == 10 and card2 == 1)):
-        profit = profit + bet * 3/2
-
-    person = Player(card1, card2, dealersCard1)
-
-    dealersCard2 = drawCard()
+    Sim = Game()
+    Result = Sim.getResult()
+    print(Result)
 
 
 if __name__ == '__main__':
